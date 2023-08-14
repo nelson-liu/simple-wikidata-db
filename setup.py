6665b23c6df3c773511a1489e6929b877828a39a
Nelson Liu <nfliu@nelsonliu.me>
2023-08-13 23:13:26 -0700

    Install requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
+import io
+import os
+
 from setuptools import setup, find_packages
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [line.strip() for line in read(path).split("\n") if not line.startswith(('"', "#", "-", "git+"))]
+
 
 setup(
     name='simple_wikidata_db',
-    version='0.0.0', 
+    version='0.0.0',
     packages=find_packages(),
+    install_requires=read_requirements("requirements.txt"),
 )
